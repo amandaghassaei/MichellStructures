b6d74a988edcc6c069fbeab5a01402c8a8bb4722
amandaghassaei <amandaghassaei@gmail.com>
2016-09-26 19:27:11 -0400

    eod

--- js/beam.js
+++ js/beam.js
@@ -2,14 +2,15 @@
  * Created by ghassaei on 9/16/16.
  */
 
-var beamMaterial = new THREE.LineBasicMaterial({color: 0x0000ff, linewidth: 3});
+//var beamMaterial = new THREE.LineBasicMaterial({color: 0x0000ff, linewidth: 3});
 
 function Beam(nodes){
 
     var lineGeometry = new THREE.Geometry();
     lineGeometry.vertices = nodes;
+    var material = new THREE.LineBasicMaterial({color: 0x0000ff, linewidth: 3});
 
-    this.line = new THREE.Line(lineGeometry, beamMaterial);
+    this.line = new THREE.Line(lineGeometry, material);
     sceneAdd(this.line);
 }
 
